e2b16184a0eeeb703c67f499e16e6daf93fd86f2
Darshan Pradhan <darshan.pradhan@anu.edu.au>
2022-10-29 20:47:18 +1100

    Module 1.

---
+++ ssg.py
@@ -0,0 +1,9 @@
+from ssg.site import Site
+import typer
+
+def main (source = "content", dest = "dist"):
+    config = {"source": source, "dest": dest}
+    Site(**config).build()
+
+
+typer.run(main)
