742c72e9182ae409cd95c161389c830d6d9df503
Darshan Pradhan <darshan.pradhan@anu.edu.au>
2022-11-01 14:31:20 +1100

    Module 5 edits.

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -41,7 +41,7 @@ class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
 
     def parse(self, path: Path, source: Path, dest: Path):
-        content = self.read(Content.load(path))
+        content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path,dest,html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
@@ -51,7 +51,7 @@ class ReStructuredTextParser(Parser):
     extensions = [".rst"]
 
     def parse(self, path: Path, source: Path, dest: Path):
-        content = self.read(Content.load(path))
+        content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name = "html5")
         self.write(path, dest, html["html_body"])
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
